352bec1e42f0e58e09a36a8e0e44ecf40372e57c
rishabhkush506-a11y <rishabhkush506@gmail.com>
2025-09-13 21:45:17 +0530

    Update Mail function

--- src/pages/Display.js
+++ src/pages/Display.js
@@ -59,13 +59,20 @@ Stress Value: ${userData?.postSliderValue || "Not answered"}
 
       <div className="button-container" style={{ marginLeft: "100px" }}>
         {/* Send Mail Button */}
-        <a
-          href={`mailto:wellbeingresearch07@gmail.com?subject=${subject}&body=${body}`}
-          className="next-button"
+        // <a
+        //   href={`mailto:wellbeingresearch07@gmail.com?subject=${subject}&body=${body}`}
+        //   className="next-button"
           
+        // >
+        //   Submit
+        // </a>
+        <button
+          onClick={() => {
+            window.location.href = `mailto:wellbeingresearch07@gmail.com?subject=${encodeURIComponent(subject)}&body=${encodeURIComponent(body)}`;
+          }}
         >
-          Submit
-        </a>
+          Sumbit
+        </button>
       </div>
     </div>
   );
